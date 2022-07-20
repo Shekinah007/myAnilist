21b0b77ea6728ed6cfd7e21b3e0a0d8887d8b635
Shekinah <shalomsheks@gmail.com>
2022-07-20 22:53:53 +0100

    Add search filter

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -23,23 +23,28 @@ const Home = () => {
     <div className="bg-blue-100 min-h-screen">
       <div className="nav bg-slate-800  h-20 flex justify-between px-4 items-center text-orange-50 text-lg">
         <h1 className="text-2xl">My Ani List</h1>
-        <input
-          className="rounded bg-blue-200"
-          placeholder="Search"
-          type="text"
-          //   value={searchItem}
-          onChange={(e) => {
-            // setSearchItem(e.target.value);
-          }}
-          onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              setPage(1);
-              //   setSearchAction((prev) => !prev);
-            }
-          }}
-        />
       </div>
       <div className="h-20 flex place-items-center justify-between px-4 overflow-scroll md:overflow-hidden gap-10 scroll-m-2 sticky top-0 bg-blue-100">
+        <div className="">
+          <p className="text-lg mb-1 text-slate-500 font-bold">Search</p>
+          <input
+            className="rounded w-40 h-12 shadow-lg bg-blue-50 p-2"
+            placeholder="Search"
+            type="text"
+            value={search}
+            onChange={(e) => {
+              if (e.target.value === "") {
+                setSearch(undefined);
+              } else setSearch(e.target.value);
+            }}
+            // onKeyDown={(e) => {
+            //   if (e.key === "Enter") {
+            //     setCurrentPage(1);
+            //     setSearchAction((prev) => !prev);
+            //   }
+            // }}
+          />
+        </div>
         <Filter
           name="Format"
           filters={format}
